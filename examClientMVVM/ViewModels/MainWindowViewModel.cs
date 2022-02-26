ef0b5c11308c0f0044f318021fe23e860aba5939
rd07 chan <rd07gamer@gmail.com>
2022-02-26 18:45:15 +0100

    Add project files.

---
+++ examClientMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace examClientMVVM.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
