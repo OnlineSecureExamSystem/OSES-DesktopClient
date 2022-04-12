3da824e914322153ecdf128a353317ed70a07de6
rd07 chan <rd07gamer@gmail.com>
2022-04-12 14:26:18 +0100

    feat : add waiting room +  the .gif files decoder

---
+++ DesktopClient/Models/Time.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopClient.Models
+{
+    public struct Time
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public Time(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public Time(TimeSpan time)
+        {
+            Hour = time.Hours;
+            Minute = time.Minutes;
+            Second = time.Seconds;
+        }
+
+        public TimeSpan ToTimeSpan()
+        {
+            return new TimeSpan(Hour, Minute, Second);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} : {1} : {2}", Hour, Minute, Second);
+        }
+
+        public static Time Parse(string time)
+        {
+            string[] parts = time.Split(':');
+            return new Time(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]));
+        }
+    }
+}
--- DesktopClient/ViewModels/WaitingRoomViewModel.cs
+++ DesktopClient/ViewModels/WaitingRoomViewModel.cs
@@ -1,4 +1,5 @@
-﻿using DesktopClient.Helpers;
+﻿using Avalonia.Threading;
+using DesktopClient.Helpers;
 using DesktopClient.Models;
 using ReactiveUI;
 using System;
@@ -11,16 +12,36 @@ namespace DesktopClient.ViewModels
 {
     public class WaitingRoomViewModel : ViewModelBase, IRoutableViewModel
     {
+        #region Properties
+
+        private TimeSpan _examTimer = new(0, 5, 5);
+        
+        public TimeSpan ExamTimer
+        {
+            get { return _examTimer; }
+            set { this.RaiseAndSetIfChanged(ref _examTimer, value); }
+        }
+
+        #endregion
 
         public string? UrlPathSegment => "/WaitingRoom";
 
+        private DispatcherTimer _timer { get; set; }
         public IScreen HostScreen { get; }
 
         public WaitingRoomViewModel(IScreen screen)
         {
             HostScreen = screen;
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
 
+        }
 
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            ExamTimer = ExamTimer.Subtract(TimeSpan.FromSeconds(1));
         }
     }
 }
